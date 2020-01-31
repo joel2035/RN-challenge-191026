fc1917e0d75cf2da066494b1d3189d1236b2bc0a
joel2035 <joel.pokam@hetic.net>
2020-01-31 09:58:46 +0100

    update info

--- Components/Info.js
+++ Components/Info.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Text, StyleSheet, View, Image } from 'react-native';
-import { Card, Image } from 'react-native-elements';
+import { Card, ListItem, Button } from 'react-native-elements';
 
 const users = [
     {
